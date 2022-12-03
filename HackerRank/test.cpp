a0ce06da109b369a8f550ace0d744f69832b1311
Alpha <syedaarishshah@gmail.com>
2022-12-04 00:14:05 +0530

    added question 3, tested question 3

--- HackerRank/test.cpp
+++ HackerRank/test.cpp
@@ -5,3 +5,19 @@ int main()
 {
     cout << "Hlo";
 }
+
+int fun()
+{
+    int d; // d is number of shifts
+    int temp;
+    for (int i = 1; i == d; i++)
+    {
+        temp = a[n]; // temp stores nth(last) element of the array
+        while (n != 1)
+        {
+            a[n] = a[n - 1];
+            n = n - 1;
+        }
+        a[0] = temp;
+    }
+}
